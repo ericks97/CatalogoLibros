dff4db7624775bd78a6b354404032992c9708932
ericks97 <erickaljndrr@gmail.com>
2025-08-06 11:17:51 -0400

    Configuración inicial del proyecto con Docker y API de Status.

---
+++ src/main/java/com/proyecto/literalura/CatalogoLibros/controller/StatusController.java
@@ -0,0 +1,17 @@
+package com.proyecto.literalura.CatalogoLibros.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class StatusController {
+    @GetMapping("/status")
+    public Map<String, String> getStatus() {
+        return Map.of("status", "Aplicación en línea y funcionando correctamente");
+    }
+}
