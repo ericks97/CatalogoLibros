1b9ebc7cc80d39297f2deba3d9475d0a9e7f9ac8
ericks97 <erickaljndrr@gmail.com>
2025-08-05 20:45:37 -0400

    firstcommit

---
+++ src/main/java/com/proyecto/literalura/CatalogoLibros/CatalogoDeLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.literalura.CatalogoLibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoDeLibrosApplication.class, args);
+	}
+
+}
